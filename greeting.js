48d5df4f951725cffa953d60f24d0d6c8ee4c3b4
Jingi Kim <copyx00@gmail.com>
2020-04-22 01:10:57 +0900

    이름 저장 기능 추가

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form");
+const input = document.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+// TODO: 이름 재설정하는 기능이 있어야할 것 같은데?!
+
+function saveName(text) {
+  localStorage.setItem("currentUser", name);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const name = input.value;
+  paintGreeting(name);
+  saveName(name);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
