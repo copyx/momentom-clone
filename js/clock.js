169e51e80457bffc3ddd2ea48472173d9e660520
Jingi Kim <copyx00@gmail.com>
2020-05-03 23:39:25 +0900

    시계 스타일 적용

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,12 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clock = document.querySelector("h1.clock");
 
-// TODO: 정확한 시간 표현 방법 고민 필요.
 function getTime() {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+
+  clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
