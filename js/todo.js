ebfd49ab624d2085cd28c80b314875b66a0c69f3
Jingi Kim <copyx00@gmail.com>
2020-05-04 00:57:30 +0900

    TODO 완료 표시 기능 추가 및 디자인 변경

--- js/todo.js
+++ js/todo.js
@@ -1,69 +1,131 @@
-const toDoForm = document.querySelector(".js-toDoForm");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-toDoList");
+const todoForm = document.querySelector(".todo__form");
+const input = todoForm.querySelector("input");
+const pendingList = document.querySelector(".todo__pending-list");
+const finishedList = document.querySelector(".todo__finished-list");
 
-const TODOS_LS = "toDos";
+const toDoList = [];
 
-let toDos = [];
+const TODO_LIST_LS = "todo-list";
 
-function deleteToDo(event) {
-  const btn = event.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanTodos = toDos.filter(function (toDo) {
-    return toDo.id !== parseInt(li.id);
+function saveToDoList() {
+  localStorage.setItem(TODO_LIST_LS, JSON.stringify(toDoList));
+}
+
+function toggleToDo(id) {
+  toDoList.forEach((toDo) => {
+    if (toDo.id === id) {
+      toDo.isCompleted = !toDo.isCompleted;
+    }
   });
-  toDos = cleanTodos;
-  saveTodos();
+  saveToDoList();
 }
 
-function saveTodos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function onClickToggleBtn(event) {
+  event.preventDefault();
+  const toggleBtn = event.target;
+  const toDoElement = toggleBtn.parentElement;
+  const iconElement = toggleBtn.querySelector("i");
+
+  if (toDoElement.classList.contains("completed")) {
+    pendingList.appendChild(toDoElement);
+  } else {
+    finishedList.appendChild(toDoElement);
+  }
+
+  toDoElement.classList.toggle("completed");
+  iconElement.classList.toggle("fa-square");
+  iconElement.classList.toggle("fa-check-square");
+
+  toggleToDo(parseInt(toDoElement.id, 10));
 }
 
-function paintTodo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "🗑";
-  delBtn.addEventListener("click", deleteToDo);
-  const span = document.createElement("span");
-  span.innerText = text;
-  const newId = toDos.length + 1;
+function onClickRemoveBtn(event) {
+  const toDoElement = event.target.parentElement;
+  toDoElement.remove();
 
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
+  const id = parseInt(toDoElement.id, 10);
+  const index = toDoList.findIndex((toDo) => toDo.id === id);
+  toDoList.splice(index, 1);
 
-  toDoList.appendChild(li);
+  saveToDoList();
+}
+
+function createTodoToggleBtn(isCompleted) {
+  const toggleBtn = document.createElement("button");
+  toggleBtn.classList.add("todo-item__toggle-btn");
+  toggleBtn.addEventListener("click", onClickToggleBtn);
+
+  const icon = document.createElement("i");
+  icon.classList.add("far");
+  icon.classList.add(isCompleted ? "fa-check-square" : "fa-square");
+  toggleBtn.appendChild(icon);
+
+  return toggleBtn;
+}
+
+function createTodoRemoveBtn() {
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("todo-item__remove-btn");
+  removeBtn.addEventListener("click", onClickRemoveBtn);
+
+  const icon = document.createElement("i");
+  icon.classList.add("far");
+  icon.classList.add("fa-trash-alt");
+  removeBtn.appendChild(icon);
+
+  return removeBtn;
+}
+
+function createToDoElement(toDoObj) {
+  const toDoElement = document.createElement("li");
+  toDoElement.innerText = toDoObj.text;
+  toDoElement.id = toDoObj.id;
+  toDoElement.classList.add("todo-item");
+
+  if (toDoObj.isCompleted) {
+    toDoElement.classList.add("completed");
+  }
+
+  const toggleBtn = createTodoToggleBtn(toDoObj.isCompleted);
+  toDoElement.prepend(toggleBtn);
+
+  const removeBtn = createTodoRemoveBtn();
+  toDoElement.appendChild(removeBtn);
+
+  return toDoElement;
+}
+
+function addToDo(toDoObj) {
+  toDoList.push(toDoObj);
+  saveToDoList();
+
+  const toDoElement = createToDoElement(toDoObj);
+  (toDoObj.isCompleted ? finishedList : pendingList).appendChild(toDoElement);
+}
+
+function onSubmitTodo(event) {
+  event.preventDefault();
 
   const toDoObj = {
-    text: text,
-    id: newId,
+    id: new Date().getTime(),
+    text: input.value,
+    isCompleted: false,
   };
 
-  toDos.push(toDoObj);
-  saveTodos();
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintTodo(currentValue);
-}
-
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function (toDo) {
-      paintTodo(toDo.text);
-    });
-  }
+  addToDo(toDoObj);
+  todoForm.reset();
 }
 
 function init() {
-  loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit);
+  const loadedToDoList = JSON.parse(localStorage.getItem(TODO_LIST_LS));
+
+  if (loadedToDoList !== null) {
+    loadedToDoList.forEach((toDoObj) => {
+      addToDo(toDoObj);
+    });
+  }
+
+  todoForm.addEventListener("submit", onSubmitTodo);
 }
 
 init();
